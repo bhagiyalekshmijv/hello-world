ccc7c9f43242abcc6e22152b144bba76d38d5007
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-05-20 11:58:30 +0530

    hello world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import image from "./images/flower.jpg"
 function App() {
+  let greeting = "Hello World";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+  <h1 className="greetingStyle">{greeting}</h1>
+  <img src={image}/>
+  </div>
   );
 }
-
 export default App;
