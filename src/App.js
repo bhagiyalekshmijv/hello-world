4cfd4179e7b406d085fa8fa568b03e8c3567c072
bhagiyalekshmijv <bhagiyalekshmijv4401@gmail.com>
2026-05-22 11:53:33 +0530

    Hello World

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
-import Child from './child';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [ dataFromChild, setDataFromChild] = useState(null);
-  
-  const handleDataFromChild = (data) => {
-    setDataFromChild(data);
-}
+  const [count, setCount] = useState(0);
 
-  return(
-      <div>
-            <Child onDataFromChild = {handleDataFromChild} />
-            <p> Data from child component : {dataFromChild} </p>
+  useEffect(() => {
+    console.log(`Count changed to ${count}`);
+    // Do something here after the count state value changes
+  }, [count]);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment Count</button>
     </div>
-    );
+  );
 }
-
 export default App;
